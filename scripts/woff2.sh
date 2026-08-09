753194bcda47afdcae53dc1752e5aca7cd8d904d
2u <a@dyyni.org>
2026-08-09 12:52:16 +0300

    chore: ttf->woff2 + unicode stripping util

---
+++ scripts/woff2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if ! command -v pyftsubset &> /dev/null; then
+    echo "[!] pyftsubset command not found, please install the 'fonttools' package"
+    exit 1
+fi
+
+[ "$#" -ne 1 ] && echo "[?] usage: $0 <INPUT_TTF>" && exit 1
+
+INPUT_TTF="$1"
+OUTPUT_WOFF="${INPUT_TTF%.*}.woff2"
+
+echo "[*] ttf->woff2 & unicode stripping ..."
+pyftsubset "$INPUT_TTF" \
+  --output-file="$OUTPUT_WOFF" \
+  --flavor=woff2 \
+  --unicodes=U+0020-007E,U+00A0,U+2010-2015,U+2018-2019,U+201C-201D \
+  --layout-features='*' \
+  --glyph-names \
+  --symbol-cmap \
+  --legacy-cmap
+echo "[+] wrote to $OUTPUT_WOFF"
+
